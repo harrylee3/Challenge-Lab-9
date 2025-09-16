36d4f42bbea6fa4a30584f23a8dae3f7848eae33
HarryLee <hlb5000@gmail.com>
2025-09-16 11:07:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Enter a number:");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+Console.WriteLine(IfNumberContains3(number));
+
+static bool IfNumberContains3 (int number)
+{
+    while (number > 0)
+    {
+        int digit = number % 10; // Get the last digit
+        if (digit == 3)
+        {
+            return true; // Found the digit 3
+        }
+        number /= 10; // Remove the last digit
+    }
+    return false; // Digit 3 not found
+}
